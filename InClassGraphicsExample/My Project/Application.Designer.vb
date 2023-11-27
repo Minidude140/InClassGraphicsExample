11e0525f2a2d8be40e559dbb18063edd9da9d82e
Zac <zac.minidude140@Gmail.com>
2023-11-27 08:27:39 -0700

    Rename Form

--- InClassGraphicsExample/My Project/Application.Designer.vb
+++ InClassGraphicsExample/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.InClassGraphicsExample.Form1
+            Me.MainForm = Global.InClassGraphicsExample.GraphicsForm
         End Sub
     End Class
 End Namespace
