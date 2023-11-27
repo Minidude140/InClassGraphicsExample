e2b2c02f47dede77f83d2d4a9434b042a9cb14c7
Zac <zac.minidude140@Gmail.com>
2023-11-27 08:37:56 -0700

    Created Form and header in code

--- InClassGraphicsExample/GraphicsForm.vb
+++ InClassGraphicsExample/GraphicsForm.vb
@@ -1,3 +1,12 @@
-﻿Public Class GraphicsForm
+﻿'Zachary Christensen
+'RCET 2265
+'Fall 2023
+'In Class Graphics Example
+'https://github.com/Minidude140/InClassGraphicsExample.git
+
+Option Strict On
+Option Explicit On
+
+Public Class GraphicsForm
 
 End Class
