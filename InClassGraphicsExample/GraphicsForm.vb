614abb399b4c65d55d16219ac65e99a650701c45
Zac <zac.minidude140@Gmail.com>
2023-11-28 08:12:54 -0700

    added comments and doc strings

--- InClassGraphicsExample/GraphicsForm.vb
+++ InClassGraphicsExample/GraphicsForm.vb
@@ -9,15 +9,25 @@ Option Explicit On
 
 Public Class GraphicsForm
     'Custom Methods
+
+    ''' <summary>
+    ''' Sets the standard background color
+    ''' </summary>
     Sub SetDefaults()
-        DrawingPictureBox.BackColor = Color.LightBlue
+        DrawingPictureBox.BackColor = Color.BlanchedAlmond
     End Sub
 
+    ''' <summary>
+    ''' Draws a line from corner to corner across the picture box
+    ''' </summary>
     Sub DrawLine()
         'initialize graphics object and set drawing surface to picture box
         Dim g As Graphics = DrawingPictureBox.CreateGraphics
+        'initialize pen as color black
         Dim pen As New Pen(Color.Black)
+        'draws a line from given co-ordinates (x,y) to (x,y); (0,0) in upper left hand corner
         g.DrawLine(pen, 10, 10, DrawingPictureBox.Width - 10, DrawingPictureBox.Height - 10)
+        'dispose of the pen and graphics object to clear memory
         pen.Dispose()
         g.Dispose()
     End Sub
